0171cfe4a4266bb491425c1607be4da53452cd76
adambournes21 <96106344+adambournes21@users.noreply.github.com>
2021-12-15 02:06:29 -0500

    Add files via upload

---
+++ Stock Algorithm & Trading Simulator/yfinance-main/import yfinance as yf.py
@@ -0,0 +1,22 @@
+import yfinance as yf
+
+def main():
+    startingCash1 = 100000
+    portfolio1 = dict()
+
+    return 42
+
+#needs to return format "2020-06-02" or "YYYY-MM-DD"
+def convertDaysAgoToDate(daysAgo, currentDay):
+
+    return 42
+msft = yf.Ticker("MSFT")
+
+# get stock info
+print(msft.history('2y'))
+print(msft.history('2y', start="2019-11-06", interval='1d'))
+print(msft.dividends)
+
+# get historical market data
+#hist = msft.history(period="2y")
+#print(hist)
